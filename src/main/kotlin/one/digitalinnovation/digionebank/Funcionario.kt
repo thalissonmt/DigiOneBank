2c58fa2d8e5ba1521de407701daad909463a9c05
Thalisson <thalissonmt@gmail.com>
2021-07-25 11:30:21 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -2,9 +2,10 @@ package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
 
-class Funcionario(
-    override val nome: String,
-    override val cpf: String,
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
     val salario: BigDecimal
 ): Pessoa(nome, cpf) {
+    abstract fun calculoAuxilio()
 }
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main(){
-    val tmt = Pessoa(nome = "Thalis", cpf = "123.123.123-47")
-    println(tmt.nome)
-    println(tmt.cpf)
-}
