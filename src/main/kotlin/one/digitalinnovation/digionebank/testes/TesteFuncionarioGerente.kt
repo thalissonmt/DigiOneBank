e7d2ca9fd234500863b23b36d4d084ae4dcc0cca
Thalisson <thalissonmt@gmail.com>
2021-07-25 12:10:42 -0300

    updated manager class and test to implement login interface

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
-    val maria = Gerente("Maria Jose","123.123.123-47", 5000.0)
+    val maria = Gerente("Maria Jose","123.123.123-47", 5000.0, "senha12")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao.autentica(maria)
 }
